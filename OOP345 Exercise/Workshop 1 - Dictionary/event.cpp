94b5c2dfd712a985baf137d33ff20c2c603744c4
Taehwa Hong <thong14@myseneca.ca>
2024-09-18 01:31:51 -0400

    add enum and some code in event module

--- OOP345 Exercise/Workshop 1 - Dictionary/event.cpp
+++ OOP345 Exercise/Workshop 1 - Dictionary/event.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "event.h"
 
 using namespace std;
 
 namespace seneca
 {
+	Event::Event() : e_eventName{nullptr}, e_duration{0}
+	{
+	}
+
+	Event::Event(const char* name, const std::chrono::nanoseconds& duration) : e_eventName{name}, e_duration{duration}
+	{
+	}
+
+	std::ostream& operator<<(std::ostream& os, const Event& ro)
+	{
+		static int COUNTER = 1;
 
 
+
+		cout.fill(' ');
+		cout.width(2);
+		cout << COUNTER++ << ": ";
+
+		cout.fill(' ');
+		cout.width(40);
+		cout << ro.e_eventName;
+
+		cout << " -> ";
+
+		switch (tUnit)
+		{
+		case seneca::Seconds:
+
+			break;
+		case seneca::Milliseconds:
+			break;
+		case seneca::Microseconds:
+			break;
+		case seneca::Nanoseconds:
+			break;
+		default:
+			break;
+		}
+
+
+		return os;
+	}
 }
--- OOP345 Exercise/Workshop 1 - Dictionary/event.h
+++ OOP345 Exercise/Workshop 1 - Dictionary/event.h
@@ -4,15 +4,25 @@
 
 namespace seneca
 {
+	enum TimeUnits
+	{
+		Seconds,
+		Milliseconds,
+		Microseconds,
+		Nanoseconds
+	};
+
+	TimeUnits tUnit = TimeUnits::Nanoseconds;
+
 	class Event
 	{
-		std::string eventName;
-		std::chrono::nanoseconds duration;
+		std::string e_eventName {};
+		std::chrono::nanoseconds e_duration{};
 
 	public:
 		Event();
 		Event(const char* name, const std::chrono::nanoseconds& duration);
-		friend std::ostream operator<<(std::ostream& os, const Event& ro);
+		friend std::ostream& operator<<(std::ostream& os, const Event& ro);
 	};
 }
 
