2f2d78a813e1345c02e5c8ccaec09a13bae9f71b
Taehwa Hong <thong14@myseneca.ca>
2024-10-10 01:15:43 -0400

    Add name and student ID

--- OOP345 Exercise/Workshop 2 - RPG Game/abilities.h
+++ OOP345 Exercise/Workshop 2 - RPG Game/abilities.h
@@ -1,3 +1,14 @@
+//*****************************************************************************
+//<assessment name example : Workshop - #2>
+//  Full Name : Taehwa Hong
+//  Student ID# : 132546227
+//	Email : thong14@myseneca.ca
+//	Section : OOP345 NDD
+//	Authenticity Declaration :
+//I declare this submission is the result of my own work and has not been
+//shared with any other student or 3rd party content provider.This submitted
+//piece of work is entirely of my own creation.
+//* ****************************************************************************
 #ifndef SENECA_ABILITIES_H
 #define SENECA_ABILITIES_H
 #include <iostream>
