6e23524bc18dc44926b9c20d16aa07c36359632c
Taehwa Hong <thong14@myseneca.ca>
2024-11-24 23:16:13 -0500

    add name and student#

--- OOP345 Exercise/Workshop 4 - Factory Assembly Line/Workstation.h
+++ OOP345 Exercise/Workshop 4 - Factory Assembly Line/Workstation.h
@@ -1,3 +1,14 @@
+//*****************************************************************************
+//<assessment name : Workshop - #4>
+//  Full Name : Taehwa Hong
+//  Student ID# : 132546227
+//	Email : thong14@myseneca.ca
+//	Section : OOP345 NDD
+//	Authenticity Declaration :
+//I declare this submission is the result of my own work and has not been
+//shared with any other student or 3rd party content provider.This submitted
+//piece of work is entirely of my own creation.
+//* ****************************************************************************
 #ifndef SENECA_WORKSTATION_H
 #define SENECA_WORKSTATION_H
 #include <string>
